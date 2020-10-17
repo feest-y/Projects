6bccf1fc359115a9afbb92eab33a976a1dd9f343
Feest <ffgfvfgrgf@gmail.com>
2020-10-17 16:03:06 +0300

    init

--- Prakt 1/Project 1/Project 1.cpp
+++ Prakt 1/Project 1/Project 1.cpp
@@ -1,7 +1,15 @@
-﻿#include <iostream>
-
-int main()
+﻿int main()
 {
-    std::cout << "Hello World!\n";
+	int a;
+	printf("size of int: %d", sizeof(int));
+	printf(" bytes\n");
+	printf("size of int: %d", sizeof(short));
+	printf(" bytes\n");
+	printf("size of int: %d", sizeof(long));
+	printf(" bytes\n");
+	printf("size of int: %d", sizeof(char));
+	printf(" bytes\n");
+	printf("size of int: %d", sizeof(float));
+	printf(" bytes");
+	return 0;
 }
-
