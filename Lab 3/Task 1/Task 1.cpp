bec3df1b1773b9abf45f6330dfd0661d14ff370a
Feest <ffgfvfgrgf@gmail.com>
2020-10-17 23:38:47 +0300

    init

---
+++ Lab 3/Task 1/Task 1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	double a = 1, b = 1, c = 1, d = 1, x = 1, y = 1, t1 = 1;
+	printf("Input x,y > ");
+	scanf_s("%lf %lf", &x, &y);
+	if ((c != 0) && (a * x + b != 0) && c != 0 && ((y * x + a) / (a * x + b)) > 0) {
+		t1 = (1 / c) * ((1 / (a * x + b) + (y / c) * log((y * x + a) / (a * x + b))));
+		printf("t1=%lf\n", t1);
+	}
+	else {
+		printf("Значения не допустимы");
+	}
+}
---
+++ Lab 3/Task 2/Task 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	double a = 1, b = 1, x = 1, y = 1, t2 = 1;
+	printf("Input x,y > ");
+	scanf_s("%lf %lf", &x, &y);
+	if ((2 * a * pow(cos(x), 2)) != 0 && 2 * a != 0 && (tan((a * x) / 2)) > 0 && cos(a * x / 2) != 0) { //ОДЗ
+		t2 = (sin(a * x)) / (2 * a * pow(cos(x), 2) + (1 / (2 * a)) * log(tan((a * x) / 2)));
+		printf("t2=%lf\n", t2);
+	}
+	else {
+		printf("Значения не допустимы");
+	}
+
+}
