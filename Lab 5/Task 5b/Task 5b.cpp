46e89d1e5794ba692057037d0775e5e1fcafea00
Feest <ffgfvfgrgf@gmail.com>
2020-10-18 16:32:52 +0300

    init

---
+++ Lab 5/Task 5b/Task 5b.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int k = 0;
+	printf("k > ");
+	scanf_s("%d", &k);
+
+	//for (int i = 0; i <= 100; i++)
+	//{
+		//k = i;
+	printf("У меня %d гриб", k);
+	if ((k > 10 && k < 20) || (k % 10 >= 5 && k % 10 <= 9) || k % 10 == 0) {
+		printf("ов\n");
+	}
+	else if (k % 10 >= 2 && k % 10 <= 4) {
+		printf("а\n");
+	}
+	else printf("\n");
+	//}
+}
