86b360c272a225f93be3787172bd7e3911844e10
Feest <ffgfvfgrgf@gmail.com>
2020-10-19 14:16:09 +0300

    red

--- Lab 6/Task 1/Task 1.cpp
+++ Lab 6/Task 1/Task 1.cpp
@@ -2,5 +2,43 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n = 10;
+	double a = (-2 * (n + 1)) / (1 + n * n), // после упрощения дроби
+		s10 = a,
+		a1 = (-2 * (1 + 1)) / (1 + 1 * 1),
+		a2 = (-2 * (2 + 1)) / (1 + 2 * 2),
+		d = a2 - a1;
+
+	for (double i = 1; i <= n; i++)
+	{
+		a = (-2 * (i + 1)) / (1 + i * i);
+		s10 += a;
+		//printf("a%d = %lf\n", (int)i, s);
+	}
+
+	printf("a10 = %lf\n", s10);
+	printf("a1 = %lf\n", a1);
+	printf("d = %lf\n", d);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*for (int i = 1; i <= n; i++)
+{
+	a = (-2 * (i + 1)) / (1 + i * i);
+	s10 += a;
+}*/
--- Prakt 1/Project 6/Project 6.cpp
+++ Prakt 1/Project 6/Project 6.cpp
@@ -5,22 +5,24 @@ int main()
 	setlocale(LC_ALL, "rus");
 	int year = 0, ySec = 365 * 24 * 3600, day = 24 * 3600;
 	long long out = 0;
+	long out_s = 0;
 	printf("Введите год - ");
 	scanf_s("%d", &year);
 	if (year % 4 == 0) {
-		out = (long long)ySec + day;
-		printf("%lld", out);
-		printf(" секунд в этом высокосном году");
+		out_s = out = (long long)ySec + day;
+		printf("\nlong long > %lld секунд в этом высокосном году", out);
+		printf("\nlong > %ld секунд в этом высокосном году", out_s);
 	}
 	else {
-		out = ySec;
-		printf("%lld", out);
-		printf(" секунд в этом не высокосном году");
+		out_s = out = ySec;
+		printf("\nlong long > %lld секунд в этом не высокосном году", out);
+		printf("\nlong > %ld секунд в этом не высокосном году", out_s);
 	}
-	out = (long long)ySec * 100 + (long long)day * (100 / 4);
-	printf("\nТакже в 100 лет - %lld", out);
-	out *= 10;
-	printf(" секунд\nА также в 1000 лет - %lld", out);
-	printf(" секунд");
+	out_s = out = (long long)ySec * 100 + (long long)day * (100 / 4);
+	printf("\n\nlong long > Также в 100 лет - %lld секунд", out);
+	printf("\nlong > Также в 100 лет - %ld секунд", out_s);
+	out_s = out *= 10;
+	printf("\n\nlong long > А также в 1000 лет - %lld секунд", out);
+	printf("\nlong > А также в 1000 лет - %ld секунд\n", out_s);
 	return 0;
 }
