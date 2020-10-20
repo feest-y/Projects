99f1862fc6381a75cbaf93f3816159369de0e11f
Feest <ffgfvfgrgf@gmail.com>
2020-10-20 20:25:54 +0300

    done?

--- Lab 6/Task 6b/Task 6b.cpp
+++ Lab 6/Task 6b/Task 6b.cpp
@@ -1,7 +1,24 @@
 ﻿#include <iostream>
+#include <time.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    return 0;
+	srand(time(NULL));
+	setlocale(LC_ALL, "rus");
+	int a = 0, b = 0, m = 0, x = 0;
+	for (int i = 1; i <= 10; i++)
+	{
+		a = (rand() / 100) % 10;
+		b = rand() % 10;
+		printf("%d. %d * %d = ", i, a, b);
+		scanf_s("%d", &x);
+		if (a * b == x)
+		{
+			m++;
+		}
+	}
+	printf("Вы набрали %d балов из 10", m);
+	return 0;
 }
+
+//Нужно ли фиксить проблему с одинаковыми примерами ?
