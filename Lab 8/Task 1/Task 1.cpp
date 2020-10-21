6395ab34c942033e97c70b4943e915aeb8f369fb
Feest <ffgfvfgrgf@gmail.com>
2020-10-21 12:44:25 +0300

    init, question

---
+++ Lab 8/Task 1/Task 1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "rus");
+	const int n = 100;
+	int Arr[n];
+	for (int i = 0; i < 100; i++)
+	{
+		Arr[i] = rand() % 100 - 50;
+		printf("Arr[%d] = %d\n", i, Arr[i]);
+	}
+}	
