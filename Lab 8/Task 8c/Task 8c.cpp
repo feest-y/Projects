d6e5decbf0a50f22c4196ec9ce309f85410c5efe
Feest <ffgfvfgrgf@gmail.com>
2020-10-21 21:11:55 +0300

    done

--- Lab 8/Task 8c/Task 8c.cpp
+++ Lab 8/Task 8c/Task 8c.cpp
@@ -1,21 +1,60 @@
 ﻿#include <iostream>
 #include <ctime>
 #include <cmath>
+using namespace std;
+void PrintArray(const int* const arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d", arr[i]);
+	}
+}
+void push_back(int*& arr, int& size, const int value)
+{
+	int* newArray = new int[size + 1];
+
+	for (short i = 0; i < size; i++)
+	{
+		newArray[i] = arr[i];
+	}
+
+	newArray[size] = value;
+
+	size++;
+
+	delete[] arr;
+
+	arr = newArray;
+}
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
 	srand(time(NULL));
-	int k = 12345670, buff = 0, i = 1;
+	int size = 0,
+		i = 1,
+		* Arr = new int[size],
+		value = 0,
+		number = 0;
 	printf("Введите число > ");
-	scanf_s("%d", &k);
-	while (true)
+	scanf_s("%d", &number);
+	while (number != 0)
 	{
+		value = number % (int)pow(10, i);
+		number -= value;
+		value = value / (int)pow(10, i - 1);
+		push_back(Arr, size, value);
 		i++;
-		buff = k % (int)pow(10, i);
-		k -= buff;
-		
 	}
+	i--;
+	value = 0;
+	for (short j = 0; j < i; j++)
+	{
+		value += Arr[j];
+	}
+	PrintArray(Arr, size);
 
+	printf("\nSum > %d", value);
 
+	printf("\nКоличество цифр > %d", i);
 }
