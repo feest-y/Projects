d8721d78f50ba5a4f49fe3279cf2b26bca4c2888
Feest <ffgfvfgrgf@gmail.com>
2020-10-21 20:32:11 +0300

    done

--- Lab 8/Task 8b/Task 8b.cpp
+++ Lab 8/Task 8b/Task 8b.cpp
@@ -16,32 +16,88 @@ void PrintArray(const int* const arr, const int size)
 	}
 	printf("\n");
 }
-void push_inside(int*& arr, int& size, const int value, int position)
+void push_inside(int*& arr, unsigned int& size, const int value, const int position)
 {
-	int* newArray = new int[size + 1];
+	int x = 0;
+	if (position > size)
+	{
+		x += position - size;
+	}
+	int* newArray = new int[size + 1 + x];
 
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < position; i++)
 	{
 		newArray[i] = arr[i];
 	}
-
-	newArray[size] = value;
-
-	size++;
-
+	newArray[position] = value;
+	for (int i = position; i < size; i++)
+	{
+		newArray[i + 1] = arr[i];
+	}
+	for (int i = size; i < position; i++)
+	{
+		newArray[i] = 0;
+	}
+	size = size + 1 + x;
 	delete[] arr;
-
 	arr = newArray;
-
 }
+void pop_inside(int*& arr, unsigned int& size, const int position)
+{
+	if (position > size)
+	{
+		printf("\nInvalid position");
+	}
+	else {
+		size--;
+		int* newArray = new int[size];
 
+		for (int i = 0; i < position; i++)
+		{
+			newArray[i] = arr[i];
+		}
+
+		for (int i = position; i < size; i++)
+		{
+			newArray[i] = arr[i+1];
+		}
+
+		delete[] arr;
+
+		arr = newArray;
+	}
+}
 
 int main()
 {
+	setlocale(LC_ALL, "rus");
 	srand(time(NULL));
-	int size = 10;
+	unsigned int position = 0, size = 10, n = 0;
+	int  value = 0;
 	int* Arr = new int[size];
 	FillArray(Arr, size);
 	PrintArray(Arr, size);
+	printf("Сколько чисел вы хотите вставить > ");
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		printf("value > ");
+		scanf_s("%d", &value);
+		printf("position > ");
+		scanf_s("%d", &position);
+		push_inside(Arr, size, value, position);
+		PrintArray(Arr, size);
+	}
+
+	printf("Сколько чисел вы хотите удалить > ");
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		printf("position > ");
+		scanf_s("%d", &position);
+		pop_inside(Arr,size,position);
+		PrintArray(Arr, size);
+	}
+
 	delete[] Arr;
 }
