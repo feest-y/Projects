c8aaf555eb7dad0ce695411a6bb0d8475ee00c86
Feest <ffgfvfgrgf@gmail.com>
2020-10-21 14:48:04 +0300

    red

--- Lab 8/Task 8b/Task 8b.cpp
+++ Lab 8/Task 8b/Task 8b.cpp
@@ -1,12 +1,47 @@
 ﻿#include <iostream>
-#include<ctime>
+#include <ctime>
+
+void FillArray(int* const arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+void PrintArray(const int* const arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d\t", arr[i]);
+	}
+	printf("\n");
+}
+void push_inside(int*& arr, int& size, const int value, int position)
+{
+	int* newArray = new int[size + 1];
+
+	for (int i = 0; i < size; i++)
+	{
+		newArray[i] = arr[i];
+	}
+
+	newArray[size] = value;
+
+	size++;
+
+	delete[] arr;
+
+	arr = newArray;
+
+}
+
+
 int main()
 {
 	srand(time(NULL));
-	int Arr[10] = {};
-	for (int i = 0; i < 10; i++)
-	{
-		Arr[i] = rand() % 100 - 50;
-		printf("Arr[%d] = %d\n", i, Arr[i]);
-	}
+	int size = 10;
+	int* Arr = new int[size];
+	FillArray(Arr, size);
+	PrintArray(Arr, size);
+	delete[] Arr;
 }
--- Lab 8/Task 8c/Task 8c.cpp
+++ Lab 8/Task 8c/Task 8c.cpp
@@ -1,1 +1,21 @@
-﻿
+﻿#include <iostream>
+#include <ctime>
+#include <cmath>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	int k = 12345670, buff = 0, i = 1;
+	printf("Введите число > ");
+	scanf_s("%d", &k);
+	while (true)
+	{
+		i++;
+		buff = k % (int)pow(10, i);
+		k -= buff;
+		
+	}
+
+
+}
