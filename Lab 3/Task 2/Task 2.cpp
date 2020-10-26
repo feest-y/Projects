74736d99864d03d30ae47892460509b903ca9f97
Feest <ffgfvfgrgf@gmail.com>
2020-10-26 15:56:35 +0200

    red

--- Lab 3/Task 2/Task 2.cpp
+++ Lab 3/Task 2/Task 2.cpp
@@ -1,15 +1,16 @@
 ﻿#include <iostream>
 #include <stdio.h>
 #include <math.h>
-
+const double PI = atan(1.0) * 4;
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	double a = 1, b = 1, x = 1, y = 1, t2 = 1;
+	double a = 1, b = 1, x = 1, y = 1, t2 = 1, xr;
 	printf("Input x,y > ");
 	scanf_s("%lf %lf", &x, &y);
-	if ((2 * a * pow(cos(x), 2)) != 0 && 2 * a != 0 && (tan((a * x) / 2)) > 0 && cos(a * x / 2) != 0) { //ОДЗ
-		t2 = (sin(a * x)) / (2 * a * pow(cos(x), 2) + (1 / (2 * a)) * log(tan((a * x) / 2)));
+	xr = x * PI / 180;
+	if ((2 * a * pow(cos(xr), 2)) != 0 && 2 * a != 0 && (tan((a * xr) / 2)) > 0 && cos(a * xr / 2) != 0) { //ОДЗ
+		t2 = (sin(a * xr)) / (2 * a * pow(cos(xr), 2) + (1 / (2 * a)) * log(tan((a * xr) / 2)));
 		printf("t2=%lf\n", t2);
 	}
 	else {
---
+++ Lab 5/Task 5a/Task 5a.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+	setlocale(LC_ALL,"rus");
+	int a, b, c, d;
+	double  x1, x2;
+	printf("ax^2 + bx + c = 0\na > ");
+	scanf_s("%d", &a);
+	printf("b > ");
+	scanf_s("%d", &b);
+	printf("c > ");
+	scanf_s("%d", &c);
+	d = b * b - (4 * a * c);
+	x1 = (-b + (float)sqrt(d)) / (2 * a);
+	if (d < 0)
+	{
+		printf("Значения некорректны");
+	}
+	else if (d > 0)
+	{
+		x2 = (-b - (float)sqrt(d)) / (2 * a);
+		printf("x1 = %lf\nx2 = %lf", x1, x2);
+	}
+	else if (d == 0) {
+		printf("x = %lf", x1);
+	}
+	else printf("Значения некорректны");
+}
