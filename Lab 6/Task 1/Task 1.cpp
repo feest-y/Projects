1bc37084d3269eba5b2ed1956c1af8eb42f3412d
Feest <ffgfvfgrgf@gmail.com>
2020-11-01 20:51:48 +0200

    done

--- Lab 6/Task 1/Task 1.cpp
+++ Lab 6/Task 1/Task 1.cpp
@@ -1,44 +1,38 @@
 ﻿#include <iostream>
-
+#include <math.h>
 int main()
 {
-	int n = 10;
-	double a = (-2 * (n + 1)) / (1 + n * n), // после упрощения дроби
-		s10 = a,
-		a1 = (-2 * (1 + 1)) / (1 + 1 * 1),
-		a2 = (-2 * (2 + 1)) / (1 + 2 * 2),
-		d = a2 - a1;
-
-	for (double i = 1; i <= n; i++)
+	const double eps = 0.000001;
+	double
+		n = 0,
+		s = 0,
+		s10 = 0,
+		// correct_a1 = (-2 * (1 + 1)) / (1 + 1 * 1),
+		// correct_a = (-2 * (n + 1)) / (1 + n * n),
+		k = 1, //(-1)^n
+		a = k * (2 * (n + 1)) / (2 + (n + 1) * (n - 1));
+	
+	while (fabs(a) >= eps)
 	{
-		a = (-2 * (i + 1)) / (1 + i * i);
-		s10 += a;
-		//printf("a%d = %lf\n", (int)i, s);
+		a = k * (2 * (n + 1)) / (2 + (n + 1) * (n - 1));
+		s += a;
+		if (n == 10)
+		{
+			s10 = s;
+		}
+		if ((int)n % 500 == 0) {
+			//printf("s%d = %1.15lf\n", (int)n, s);
+			//printf("a%d = %1.15lf\n", (int)n, a);
+		}
+		n++;
+		k *= -1;
 	}
 
-	printf("a10 = %lf\n", s10);
-	printf("a1 = %lf\n", a1);
-	printf("d = %lf\n", d);
+	printf("s10 = %1.10lf\n", s10);
+	printf("s%d = %1.15lf\n", (int)n, s);
+	printf("a%d = %1.19lf\n", (int)n, a);
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*for (int i = 1; i <= n; i++)
-{
-	a = (-2 * (i + 1)) / (1 + i * i);
-	s10 += a;
-}*/
+// после упрощения дроби (-2 * (n + 1)) / (1 + n * n)
