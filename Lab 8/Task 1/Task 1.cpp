979a1ecc678a31623efac44b3fd172ce7660b0e7
Feest <ffgfvfgrgf@gmail.com>
2020-11-01 21:37:56 +0200

    done

--- Lab 8/Task 1/Task 1.cpp
+++ Lab 8/Task 1/Task 1.cpp
@@ -1,16 +1,44 @@
 ﻿#include <iostream>
 #include <time.h>
-#include <ctime>
+#include <math.h>
+void PrintArray(const int* const Arr, const int size = 100)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("Arr[%d] = %d\n", i, Arr[i]);
+	}
+	printf("\n");
+}
 int main()
 {
 	srand(time(NULL));
 	setlocale(LC_ALL, "rus");
 	const int n = 100;
-	int Arr[n];
+	int Arr[n], a = 0;
 
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < n; i++)
 	{
 		Arr[i] = rand() % 100 - 50;
-		printf("Arr[%d] = %d\n", i, Arr[i]);
 	}
+	PrintArray(Arr);
+	a = Arr[0];
+		for (int i = 0; i < n; i++)
+	{
+		if (Arr[i] < a)
+		{
+			a = Arr[i];
+			if (a == -50)
+			{
+				i = 100;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		if (Arr[i] > 0)
+		{
+			Arr[i] = abs(a);
+		}
+	}
+	PrintArray(Arr);
 }
