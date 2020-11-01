5fd8cb68fe39a873395ff7adeecbdd3f981679b5
Feest <ffgfvfgrgf@gmail.com>
2020-11-01 23:48:56 +0200

    working

--- Lab_10/Task 1/Task 1.cpp
+++ Lab_10/Task 1/Task 1.cpp
@@ -1,10 +1,11 @@
 ﻿#include <iostream>
 #include<cmath>
-void FillArray(int* const arr, const int SIZE = 8)
+#include<ctime>
+void FillArray(int* const arr, const int SIZE = 9)
 {
 	for (int i = 0; i < SIZE; i++)
 	{
-		arr[i] = rand() % 10;
+		arr[i] = rand() % 9 + 1;
 	}
 }
 void FillArray(int* const arr[], const int ROWS = 3, const int COLLS = 3)
@@ -13,11 +14,11 @@ void FillArray(int* const arr[], const int ROWS = 3, const int COLLS = 3)
 	{
 		for (int j = 0; j < COLLS; j++)
 		{
-			arr[i][j] = rand() % 99 + 1;
+			arr[i][j] = rand() % 9 + 1;
 		}
 	}
 }
-void PrintArray(const int* const arr, const int size = 8)
+void PrintArray(const int* const arr, const int size = 9)
 {
 	for (int i = 0; i < size; i++)
 	{
@@ -38,16 +39,50 @@ void PrintArray(const int* const arr[], const int ROWS = 3, const int COLLS = 3)
 	printf("\b");
 }
 
-void ArrayToMatrix(const int* const Arr, const int* const Brr[], const int SIZE = 8) {
-
+void ArrayToMatrix(int* const Arr, int* const Brr[], const int SIZE = 9) {
+	int rNc = (int)ceil(sqrt(SIZE)), k = 0, a = 0;
+	for (int i = 0; i < rNc; i++)
+	{
+		k = rNc * i;
+		for (int j = 0; j < rNc; j++)
+		{
+			if (a < SIZE) {
+				Brr[i][j] = Arr[j + k];
+			}
+			else Brr[i][j] = 0;
+			a++;
+		}
+	}
+}
+void MatrixToArray(int* const Arr, int* const Brr[], const int ROWS = 3, const int COLLS = 3, const int SIZE = 9) {
+	int k = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		k = ROWS * i;
+		for (int j = 0; j < COLLS; j++)
+		{
+			Arr[j + k] = Brr[i][j];
+		}
+	}
 }
 int main()
 {
-	int ROWS = 3, COLLS = 3, SIZE = 8;
-	int* Arr = new int[SIZE];
-	int** Brr = new int* [ROWS];
-	for (int i = 0; i < COLLS; i++)
-		Brr[i] = new int[COLLS];
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	int rows, colls, size;
+	printf("Введите размерость массива > ");
+	scanf_s("%d", &size);
+	colls = (int)ceil(sqrt(size));
+	rows = size / colls;
 
-	FillArray(Arr);
+	int* Arr = new int[size];
+	int** Brr = new int* [colls];
+	for (int i = 0; i < colls; i++)
+		Brr[i] = new int[colls];
+
+	FillArray(Brr, rows, colls);
+	PrintArray(Brr, rows, colls);
+	delete[]Arr;
+	for (int i = 0; i < colls; i++) delete[] Brr[i];
+	delete[]Brr;
 }
