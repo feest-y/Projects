221f883c84b0a61c756faad4cfcf09f544f36062
Feest <ffgfvfgrgf@gmail.com>
2020-11-02 14:33:16 +0200

    in process

--- Lab 9/Task 9a/Task 9a.cpp
+++ Lab 9/Task 9a/Task 9a.cpp
@@ -1,6 +1,89 @@
 ﻿#include <iostream>
+#include <ctime>
+
+void FillArray(int* const arr, const int SIZE = 9)
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % 9 + 1;
+	}
+}
+void FillArray(int* const arr[], const int ROWS = 9, const int COLLS = 9)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLLS; j++)
+		{
+			arr[i][j] = rand() % 9 + 1;
+		}
+	}
+}
+void PrintArray(const int* const arr, const int size = 9)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d\t", arr[i]);
+	}
+	printf("\n");
+}
+void PrintArray(const int* const arr[], const int ROWS = 9, const int COLLS = 9)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLLS; j++)
+		{
+			printf("%2.d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\b");
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(NULL));
+	int ROWS = 3, COLLS = 3;
+	int** Arr = new int* [ROWS];
+	for (int i = 0; i < COLLS; i++) Arr[i] = new int[COLLS];
+	int Buff = 10, index = 0, k = 0;
+	FillArray(Arr, ROWS, COLLS);
+	PrintArray(Arr, ROWS, COLLS);
+	for (int i = 0; i < ROWS; i++)
+	{
+		Buff = 0;
+		for (int j = 0; j < COLLS; j++)
+		{
+			if (Arr[i][j] > Buff)
+			{
+				Buff = Arr[i][j];
+				index = j;
+			}
+		}
+		Arr[i][0] = Arr[i][index];
+	}
+	printf("\nMax in ROWS");
+	printf("\n");
+	PrintArray(Arr, ROWS, COLLS);
+	Buff = 10;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLLS; j++)
+		{
+			if (Arr[j][i] < Buff)
+			{
+				Buff = Arr[j][i];
+				index = i;
+			}
+		}
+		Buff[index][i] = Buff[9][i];
+	}
+
+	printf("\nMin in COLLS");
+	printf("\n");
+	PrintArray(Arr, ROWS, COLLS);
+
+
+	for (int i = 0; i < ROWS; i++) delete[] Arr[i];
+	delete[]Arr;
+	return 0;
 }
--- Lab 9/Task 9b/Task 9b.cpp
+++ Lab 9/Task 9b/Task 9b.cpp
@@ -1,6 +1,60 @@
 ﻿#include <iostream>
+#include <ctime>
+void FillArray(int* const arr[], const int ROWS = 9, const int COLLS = 9)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLLS; j++)
+		{
+			arr[i][j] = rand() % 9 + 1;
+		}
+	}
+}
+void PrintArray(const int* const arr[], const int ROWS = 9, const int COLLS = 9)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLLS; j++)
+		{
+			printf("%2.d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\b");
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "rus");
+	srand(time(0));
+	int rows_A, rows_B, colls_A, colls_B;
+	printf("Введите размерность матрицы А:\n\tСтроки > ");
+	scanf_s("%d", &rows_A);
+	printf("\tСтолбцы > ");
+	scanf_s("%d", &colls_A);
+
+	printf("\nВведите размерность матрицы B:\n\tСтроки > ");
+	scanf_s("%d", &rows_B);
+	printf("\tСтолбцы > ");
+	scanf_s("%d", &colls_B);
+
+	int** Arr = new int* [colls_A];
+	for (int i = 0; i < colls_A; i++) Arr[i] = new int[rows_A];
+
+
+	int** Brr = new int* [colls_B];
+	for (int i = 0; i < colls_B; i++) Brr[i] = new int[rows_B];
+
+
+	FillArray(Arr,rows_A,colls_A);
+	FillArray(Brr,rows_B,colls_B);
+	printf("\nArr:\n");
+	PrintArray(Arr, rows_A, colls_A);
+	printf("\n");
+	printf("\n");
+	printf("Brr:\n");
+	PrintArray(Brr, rows_B, colls_B);
+
+
+
 }
