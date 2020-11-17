de68fc1dd70f5bcc7ae018baf70689331cc78f23
Feest <ffgfvfgrgf@gmail.com>
2020-11-17 16:49:20 +0200

    done

--- Lab_10/Task 10a/Task 10a.cpp
+++ Lab_10/Task 10a/Task 10a.cpp
@@ -1,6 +1,24 @@
 ﻿#include <iostream>
 
+
+char Compare(int a, int b) {
+	if (a < b) return '<';
+	else if (a > b) return '>';
+	else if (a == b) return '=';
+	else return '#';
+}
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "rus");
+	int a, b;
+	printf("Введите a: ");
+	scanf_s("%d", &a);
+	printf("Введите b: ");
+	scanf_s("%d", &b);
+	system("cls");
+	printf("a = %d\nb = %d\n", a, b);
+	printf("a %c b", Compare(a, b));
+	return 0;
 }
