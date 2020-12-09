d50476212b5abfd3c88707b96bf878e992ceec6d
Feest <ffgfvfgrgf@gmail.com>
2020-12-09 19:48:59 +0200

    init

---
+++ РГЗ/РГЗ/Header.h
@@ -0,0 +1,245 @@
+#pragma once
+#include<iostream>
+#include<chrono>
+#include<cmath>
+#include<ctime>
+using namespace std;
+void Standart() {
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+}
+
+void FillArray(int* const arr, const int size, const int min = 1, const int max = 99) {
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() * (max - min) + min;
+	}
+}
+
+void FillArray(int** const arr, const int ROWS, const int COLLS, const int min = 1, const int max = 99) {
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLLS; j++)
+		{
+			arr[i][j] = rand() * (max - min) + min;
+		}
+	}
+}
+
+void PrintArray(const int* const arr, const int size) {
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d\t", arr[i]);
+	}
+	printf("\n");
+}
+
+void PrintArray(const int** const arr, const int ROWS, const int COLLS) {
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLLS; j++)
+		{
+			printf("%2.d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\b");
+}
+
+//void push_inside(int*& arr, unsigned int& size, const int value, const unsigned int position){
+//	int x = 0;
+//	if (position > size)
+//	{
+//		x += position - size;
+//	}
+//	int* newArray = new int[size + 1 + x];
+//
+//	for (unsigned int i = 0; i < position; i++)
+//	{
+//		newArray[i] = arr[i];
+//	}
+//	newArray[position] = value;
+//	for (unsigned int i = position; i < size; i++)
+//	{
+//		newArray[i + 1] = arr[i];
+//	}
+//	for (unsigned int i = size; i < position; i++)
+//	{
+//		newArray[i] = 0;
+//	}
+//	size = size + 1 + x;
+//	delete[] arr;
+//	arr = newArray;
+//}
+
+//void pop_inside(const int*& const arr, unsigned int& size, int position){
+//	size--;
+//	int* newArray = new int[size];
+//	for (int i = 0; i < position; i++)
+//	{
+//		newArray[i] = arr[i];
+//	}
+//
+//	for (unsigned int i = position; i < size; i++)
+//	{
+//		newArray[i] = arr[i + 1];
+//	}
+//
+//	delete[] arr;
+//	arr = newArray;
+//	newArray = nullptr;
+//}
+
+void PushBack(int*& arr, int& size, const int value) {
+	int* newArray = new int[size + 1];
+
+	for (short i = 0; i < size; i++)
+	{
+		newArray[i] = arr[i];
+	}
+
+	newArray[size] = value;
+
+	size++;
+
+	delete[] arr;
+
+	arr = newArray;
+}
+
+void TurnArrayLeft(int* const arr[], const int ROWS = 9, const int COLLS = 9) {
+	int Buffer[9][9] = {};
+	for (int i = 0; i < ROWS; i++)
+	{
+
+		for (int j = 0; j < COLLS; j++)
+		{
+			Buffer[i][j] = arr[j][ROWS - i - 1];
+		}
+	}
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLLS; j++)
+		{
+			arr[i][j] = Buffer[i][j];
+		}
+	}
+}
+
+void ArrayToMatrix(int* const Arr, int* const Brr[], const int SIZE) {
+	int rNc = (int)ceil(sqrt(SIZE)), k = 0, a = 0;
+	for (int i = 0; i < rNc; i++)
+	{
+		k = rNc * i;
+		for (int j = 0; j < rNc; j++)
+		{
+			if (a < SIZE) {
+				Brr[i][j] = Arr[j + k];
+			}
+			else Brr[i][j] = 0;
+			a++;
+		}
+	}
+}
+
+void MatrixToArray(int* const Brr[], int* const Arr, const int ROWS, const int COLLS) {
+	int k = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		k = ROWS * i;
+		for (int j = 0; j < COLLS; j++)
+		{
+			Arr[j + k] = Brr[i][j];
+		}
+	}
+}
+
+void MatrixMultiply(int* const Arr[], const int rows_A, const int colls_A, int* const Brr[], const int rows_B, const int colls_B) {
+	int rows_C = rows_A, colls_C = colls_B;
+	int** Crr = new int* [rows_C];
+	for (int i = 0; i < rows_C; i++)
+		Crr[i] = new int[colls_C];
+
+	for (int i = 0; i < rows_C; i++)
+	{
+		for (int j = 0; j < colls_C; j++)
+		{
+			Crr[i][j] = 0;
+			for (int a = 0; a < colls_A; a++)
+			{
+				Crr[i][j] += Arr[i][a] * Brr[a][j];
+			}
+		}
+	}
+
+	//Вывод
+
+	for (int i = 0; i < rows_C; i++)
+	{
+		for (int j = 0; j < colls_C; j++)
+		{
+			printf("%3.d ", Crr[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\b");
+}
+
+template <typename T>
+void Bin(T num = 0) {
+	if (num != 0)
+	{
+		unsigned int size = 0, i = 0;
+		int* Arr = new int[size];
+		while (num)
+		{
+			push_inside(Arr, size, num % 2, i);
+			num /= 2;
+		}
+		printf(" ( ");
+		for (int i = size - 1; i >= 0; i--)
+		{
+			printf("%d", Arr[i]);
+		}
+		printf(" ) ");
+		delete[] Arr;
+	}
+	else printf(" ( 0 ) ");
+}
+
+void SwapWithPrevious(int* const arr, const int position) {
+	int buff;
+	buff = arr[position];
+	arr[position] = arr[position - 1];
+	arr[position - 1] = buff;
+}
+
+double ShakerSort(int* const arr, const int size) {
+	int leftMark = 1;
+	int rightMark = size - 1;
+	cout << endl;
+	auto start = chrono::high_resolution_clock::now();
+	while (leftMark <= rightMark)
+	{
+		for (int i = rightMark; i >= leftMark; i--)
+			if (arr[i - 1] > arr[i]) SwapWithPrevious(arr, i);
+		leftMark++;
+
+		for (int i = leftMark; i <= rightMark; i++)
+			if (arr[i - 1] > arr[i]) SwapWithPrevious(arr, i);
+		rightMark--;
+		//cout << "\nИтерация: " << leftMark - 1; // просмотр количества итераций
+	}
+	chrono::duration <double> duration = chrono::high_resolution_clock::now() - start;
+	cout << endl;
+	return duration.count();
+}
+
+void DeleteArray(int* arr) {
+	delete[] arr;
+}
+
+void DeleteArray(int** arr, const int rows) {
+	for (int i = 0; i < rows; i++) delete[] arr[i];
+	delete[] arr;
+}
---
+++ РГЗ/РГЗ/РГЗ.cpp
@@ -0,0 +1,28 @@
+﻿#include "Header.h"
+
+int main()
+{
+	Standart();
+
+	int size = 0;
+	double time = 0;
+	cout << "Размер массива: ";
+	cin >> size;
+	int* Arr = new int[size];
+
+	for (int k = 0; k < size; k++)
+	{
+		Arr[k] = size - k; // запись значений по убыванию
+	}
+	PrintArray(Arr, size);
+
+	time = ShakerSort(Arr, size); // сортировка
+
+	cout << "\nМассив после сортировки:\n";
+
+	PrintArray(Arr, size);
+	printf("\nВремя на сортировку: %.8lf", time);
+
+	DeleteArray(Arr);
+	return 0;
+}
