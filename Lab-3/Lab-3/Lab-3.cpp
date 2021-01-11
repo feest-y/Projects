b10bf6a18bcbf9bb89d92d7133851314b766d28d
Feest <ffgfvfgrgf@gmail.com>
2021-01-11 19:30:49 +0200

    done

--- Lab-3/Lab-3/Lab-3.cpp
+++ Lab-3/Lab-3/Lab-3.cpp
@@ -1,9 +1,32 @@
 ﻿#include "../../Header/Header.h"
 
+
 int main()
 {
 	Standart();
 
-
+	int size = InputSize(),n=0;
+	int* Arr = new int[size];
+	
+	FillArray(Arr,size,-100,100);
+	cout << "Arr random numbers >\n";
+	PrintArray(Arr,size);
+	
+	DescendingSort(Arr,size);
+	cout << "Arr sorted >\n";
+	PrintArray(Arr,size);
+	
+	n = CountNegativeDigits(Arr,size);
+	int* Brr = new int[n];
+	
+	for (int i = 0; i < n; i++)
+	{
+		Brr[i] = Arr[size-n+i];
+	}
+	cout << "New Brr initialized >\n";
+	PrintArray(Brr,n);
+	
+	DeleteArray(Arr);
+	DeleteArray(Brr);
 	r0;
 }
