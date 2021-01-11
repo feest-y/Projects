27ffb51eeb1ba65ee6dac093d0f3420716dde391
Feest <ffgfvfgrgf@gmail.com>
2021-01-11 18:35:43 +0200

    done

--- Lab_11/Lab_11/Lab_11.cpp
+++ Lab_11/Lab_11/Lab_11.cpp
@@ -1,7 +1,22 @@
-﻿#include <iostream>
-using namespace std;
+﻿#include "..\..\РГЗ\РГЗ\Header.h"
+
+int Di(const int* const Arr, const int size, int position = 0, int counter = 0) {
+	if (Arr[position] > position) counter++;
+	position++;
+
+	if (position < size)
+	{
+		return Di(Arr, size, position,counter);
+	}
+
+	return counter;
+}
 
 int main()
 {
-	cout << "Hello World!\n";
+	Standart();
+	int size = 10;
+	int* Arr = new int[size];
+	FillArray(Arr, size, 0, 10);
+	cout << Di(Arr, size);
 }
