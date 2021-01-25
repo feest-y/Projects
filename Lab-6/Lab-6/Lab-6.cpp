8150d46adf9a05862988eb66a6439b9ad7162e09
Feest <ffgfvfgrgf@gmail.com>
2021-01-25 22:52:50 +0200

    red

--- Lab-6/Lab-6/Lab-6.cpp
+++ Lab-6/Lab-6/Lab-6.cpp
@@ -4,15 +4,42 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "ru");
-	int length = 0;
-	cout << "¬ведите длинну первой строки > ";
-	cin >> length;
+	int length = 1000;
+	//cout << "¬ведите длинну первой строки > ";
+	//cin >> length;
 	char* Str1 = new char[length];
-
-	cout << "¬ведите длинну второй строки > ";
-	cin >> length;
+	//cout << "¬ведите длинну второй строки > ";
+	//cin >> length;
 	char* Str2 = new char[length];
 
+	//Str1 = Str2 = '\0';
+	cout << "Str1 > ";
+	cin >> Str1;
+	cout << "Str2 > ";
+	cin >> Str2;
+	int i = 0, j = 0, counter = 0, counter1 = 0;
+
+
+
+	while (Str1[i] != '\0')
+	{
+		counter = j = 0;
+		while (Str2[j] != '\0')
+		{
+			if (Str1[i] == Str2[j])
+			{
+				counter++;
+				break;
+			}
+			j++;
+		}
+		if (counter == 0)
+		{
+			cout << Str1[i];
+			break;
+		}
+		i++;
+	}
 
 	delete[] Str1;
 	delete[] Str2;
