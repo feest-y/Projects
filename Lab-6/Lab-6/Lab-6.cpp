560f01d1317f7cc86d2da72243d10d1b87cbc0d0
Feest <ffgfvfgrgf@gmail.com>
2021-01-25 20:20:03 +0200

    init

--- Lab-6/Lab-6/Lab-6.cpp
+++ Lab-6/Lab-6/Lab-6.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 
 int main() {
+	setlocale(LC_ALL, "ru");
+	int length = 0;
+	cout << "¬ведите длинну первой строки > ";
+	cin >> length;
+	char* Str1 = new char[length];
 
+	cout << "¬ведите длинну второй строки > ";
+	cin >> length;
+	char* Str2 = new char[length];
+
+
+	delete[] Str1;
+	delete[] Str2;
 	return 0;
 }
