866038a9d103cc351afb7e0f636a6472c1e0b01c
Feest <ffgfvfgrgf@gmail.com>
2021-01-25 23:35:03 +0200

    red

--- Lab-6/Lab-6/Lab-6.cpp
+++ Lab-6/Lab-6/Lab-6.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
+#include <String>
 using namespace std;
 
 
 int main() {
+
 	setlocale(LC_ALL, "ru");
 	int length = 1000;
-	//cout << "¬ведите длинну первой строки > ";
-	//cin >> length;
+	string input;
+
+	cout << "¬ведите длинну первой строки > ";
+	while (cin >> length)
+	{
+
+	}
 	char* Str1 = new char[length];
-	//cout << "¬ведите длинну второй строки > ";
-	//cin >> length;
+
+	cout << "¬ведите длинну второй строки > ";
+	while (cin >> length)
+	{
+
+	}
 	char* Str2 = new char[length];
 
-	//Str1 = Str2 = '\0';
+
 	cout << "Str1 > ";
 	cin >> Str1;
 	cout << "Str2 > ";
 	cin >> Str2;
 	int i = 0, j = 0, counter = 0, counter1 = 0;
-
-
 
 	while (Str1[i] != '\0')
 	{
