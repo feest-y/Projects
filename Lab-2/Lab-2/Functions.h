a947d9eb9980dcbdcb54cecafc6dbf047fe69cd4
Feest <ffgfvfgrgf@gmail.com>
2021-01-26 00:56:39 +0200

    done

---
+++ Lab-2/Lab-2/Functions.h
@@ -0,0 +1,63 @@
+#pragma once
+#include "Company.h"
+#include "Debug.h"
+
+void PrintS(Company All, int n = 10);
+void SortS(Company All, int n = 10);
+
+void PrintS(Company All, int n) {
+
+	std::cout << "\n\n";
+	printf("---------------------------------------------\n");
+	printf("|      Сельскохозяйственные предприятия      |\n");
+	printf("|--------------------------------------------|\n");
+	printf("|  Название |  Вид  |Количество|   Площадь   |\n");
+	printf("|           |       |работников|   земли (га)|\n");
+	printf("|-----------|-------|----------|-------------|\n");
+
+	for (short i = 0; i < n; i++)
+	{
+		A[i].PrintCompany();
+	}
+
+	printf("----------------------------------------------\n");
+
+#ifdef DEBUG
+	std::cout << __FILE__ << " > " << __FUNCTION__ << ": ";
+	Debug();
+#endif // DEBUG
+}
+
+void SortS(Company All, int n) {
+
+	Company Buffer = A[0];
+	for (int i = 0; i < n - 1; i++) {
+		if (A[i].name > A[i + 1].name) {
+			Buffer = A[i + 1];
+			A[i + 1] = A[i];
+			A[i] = Buffer;
+			i = 0;
+		}
+	}
+
+	/*int Arr[10] = { 1,424,4124,5,124,5,547,123,24,12 };
+	int buff = Arr[0];
+	for (int i = 0; i < 9; i++){
+		if (Arr[i]>Arr[i+1]){
+			buff = Arr[i + 1];
+			Arr[i + 1] = Arr[i];
+			Arr[i] = buff;
+			i = 0;
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << Arr[i] << endl;
+	}*/
+
+#ifdef DEBUG
+	std::cout << __FILE__ << " > " << __FUNCTION__ << ": ";
+	Debug();
+#endif // DEBUG
+}
---
+++ Lab-2/Lab-2/Lab-2.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//#define DEBUG
+#define PRINT_TYPE
+
+#include "Company.h"
+#include "Functions.h"
+#include "Debug.h"
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	Company All;
+	short n = 10;
+
+	for (short i = 0; i < n; i++)
+	{
+#ifdef PRINT_TYPE
+		A[i].SetCompany(true);
+#else
+		if (i != 0) std::cout << "\n";
+		std::cout << "Company " << i + 1 << ":\n";
+		A[i].SetCompany(false);
+#endif // PRINT_TYPE
+
+	}
+
+	PrintS(All);
+
+	SortS(All);
+
+	PrintS(All);
+#ifdef DEBUG
+	std::cout << __FILE__ <<" > "<< __FUNCTION__ << ": ";
+	Debug();
+#endif // DEBUG
+	return 0;
+}
