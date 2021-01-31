62bb5e23d4632a1bc2b09eed3c731ed6728ea024
Feest <ffgfvfgrgf@gmail.com>
2021-01-31 21:33:24 +0200

    red

--- Lab-7/Lab-7/Company.h
+++ Lab-7/Lab-7/Company.h
@@ -3,13 +3,31 @@
 #include <fstream>
 #include <cstdio>
 #include <String>
-#include "Debug.h"
-#include "Functions.h"
-
 #define zxc printf("|")
 #define zxcf fout.width(1);fout << "|"
 
 using namespace std;
+
+
+int StrToInt(string b) {
+	int x = 0, counter = 0;;
+	for (int i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] >= 48 && b[i] <= 57 && counter == 0)
+		{
+			x *= 10;
+			x += (b[i] - 48);
+		}
+		/*else if (b[i] == 46 && b[i + 1] >= 48 && b[i + 1] <= 57) {
+			counter++;
+		}
+		else if (counter != 0 && b[i] >= 48 && b[i] <= 57) {
+			x += (b[i] - 48)/(pow(10,counter));
+			counter++;
+		}*/
+	}
+	return x;
+}
 
 struct Company
 {
