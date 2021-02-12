799bc0e17991830223b2667937c34997579c52ce
Feest <ffgfvfgrgf@gmail.com>
2021-02-12 11:01:04 +0200

    mod

--- Lab-3b/Lab-3b/Lab-3b.cpp
+++ Lab-3b/Lab-3b/Lab-3b.cpp
@@ -7,7 +7,7 @@ int main()
 	int* Arr = new int[size] {};
 	//PrintArray(Arr, size);
 	FillArray(Arr, size, -10, 10);
-//	PrintArray(Arr, size);
+	//	PrintArray(Arr, size);
 	n = CountNegativeDigits(Arr, size);
 	p = CountPossitiveDigits(Arr, size);
 	z = size - n - p;
@@ -19,24 +19,24 @@ int main()
 	int* Nrr = new int[n] {};
 	for (int i = 0; i < n; i++)
 	{
-		Nrr[i] = Arr[size - n + i];
+		*(Nrr + i) = *(Arr + (size - n + i));
 	}
 
 	int* Prr = new int[p] {};
 	for (int i = 0; i < p; i++)
 	{
-		Prr[i] = Arr[i];
+		*(Prr + i) = *(Arr + i);
 	}
 
 	int* Zrr = new int[z] {};
 	for (int i = 0; i < z; i++)
 	{
-		Zrr[i] = Arr[p+i];
+		*(Zrr + i) = *(Arr + p + i);
 	}
 
-	PrintArray(Prr,p);
-	PrintArray(Zrr,z);
-	PrintArray(Nrr,n);
+	PrintArray(Prr, p);
+	PrintArray(Zrr, z);
+	PrintArray(Nrr, n);
 
 	DeleteArray(Arr);
 	DeleteArray(Zrr);
