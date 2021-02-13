1ef82e7e3959cda0a53eaf99a5e17c7fb63a3d05
Feest <ffgfvfgrgf@gmail.com>
2021-02-13 22:54:50 +0200

    ndone

--- Lаb-10в/Lаb-10в/Lаb-10в.cpp
+++ Lаb-10в/Lаb-10в/Lаb-10в.cpp
@@ -1,8 +1,25 @@
 ﻿#include "..\..\Header\Header.h"
 
+
+class Sum {
+public:
+	int x = 0, y = 0, s = 0;
+	void Init() {
+		x = InputSize("x > ");
+		y = InputSize("y > ");
+	}void Print() {
+		cout << "\nx > " << x << endl << "y > " << y << endl << "s > " << s << endl;
+	}void CountS() {
+		s = x + y;
+		//cout << s << endl;
+	}
+};
+
 int main()
 {
 	Standart();
-
+	Sum A;
+	A.Init();
+	A.Print();
 	return 0;
 }
