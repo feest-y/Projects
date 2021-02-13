224f2ac7266b60d540646653620178804fad3636
Feest <ffgfvfgrgf@gmail.com>
2021-02-13 21:45:56 +0200

    done

---
+++ Lаb-10a/Lаb-10a/Lаb-10a.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+T Square(T a, T b = '\0') {
+	if (b == '\0')
+		return a * a;
+	else
+		return a * b;
+}
+
+int main()
+{
+	cout << Square(6);
+	return 0;
+}
