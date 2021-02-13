6793661abc96f2342de51cf35b2cf3a670bc33e0
Feest <ffgfvfgrgf@gmail.com>
2021-02-13 23:06:33 +0200

    init

---
+++ Lаb-10г/Lаb-10г/Lаb-10г.cpp
@@ -0,0 +1,20 @@
+﻿#include "..\..\Header\Header.h"
+
+
+class Timer {
+public:
+	int sec;
+	Timer(int sec = 0, int min = 0) {
+		this->sec = sec + min * 60;
+	};
+
+	void Print() {
+		cout << sec << endl;
+	}
+};
+int main()
+{
+	Timer A = (1, 2);
+	A.Print();
+	r0;
+}
