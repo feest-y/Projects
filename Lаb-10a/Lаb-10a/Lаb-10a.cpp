ad73c09d5832e460d753f9f057a9ee825b0021be
Feest <ffgfvfgrgf@gmail.com>
2021-03-04 17:27:10 +0200

    sdone

--- Lаb-10a/Lаb-10a/Lаb-10a.cpp
+++ Lаb-10a/Lаb-10a/Lаb-10a.cpp
@@ -1,16 +1,19 @@
 ﻿#include <iostream>
 using namespace std;
-
-template <typename T>
-T Square(T a, T b = '\0') {
-	if (b == '\0')
-		return a * a;
-	else
-		return a * b;
-}
+class Sq {
+public:
+	template <typename T>
+	T Square(T a, T b = '\0') {
+		if (b == '\0')
+			return a * a;
+		else
+			return a * b;
+	}
+};
 
 int main()
 {
-	cout << Square(6);
+	Sq A;
+	cout << A.Square(6);
 	return 0;
 }
