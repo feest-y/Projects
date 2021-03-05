7e54a9a47b94a05de2a393976419b914f188e871
Feest <ffgfvfgrgf@gmail.com>
2021-03-05 10:44:52 +0200

    init

---
+++ Lab-7б/Lab-7б/Lab-7б.cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+
+void FillArray(int* const arr, const int size, const int min = 0, const int max = 9) {
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % (max - min) + min;
+	}
+}
+
+int main()
+{
+	FILE* file;
+	int Arr[10], a[10];
+
+	FillArray(a, 10);
+
+
+	if ((file = fopen("test", "w")) == NULL) {
+		printf("Cannot open file.\n");
+		exit(1);
+	}
+
+	if (fwrite(&a, sizeof(int), 1, file) != 10) {
+		if (feof(file)) printf("End of file.");
+		else printf("Read error.");
+	}
+	fclose(file);
+
+	if ((file = fopen("test", "r")) == NULL) {
+		printf("Cannot open file.\n");
+		exit(1);
+	}
+
+	if (fread(Arr, sizeof(int), 10, file) != 10) {
+		if (feof(file)) printf("End of file.");
+		else printf("Read error.");
+	}
+
+	fclose(file);
+	return 0;
+}
