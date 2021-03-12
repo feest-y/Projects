6c2f688ac7db05712995f1a3424cf865fc258671
Feest <ffgfvfgrgf@gmail.com>
2021-03-12 17:30:32 +0200

    red

--- Lаb-10в/Lаb-10в/Lаb-10в.cpp
+++ Lаb-10в/Lаb-10в/Lаb-10в.cpp
@@ -2,24 +2,48 @@
 
 
 class Sum {
+private:
+	int* x, * y, * sum;
 public:
-	int x = 0, y = 0, s = 0;
 	void Init() {
-		x = InputSize("x > ");
-		y = InputSize("y > ");
-	}void Print() {
-		cout << "\nx > " << x << endl << "y > " << y << endl << "s > " << s << endl;
-	}void CountS() {
-		s = x + y;
-		//cout << s << endl;
+		*x = InputSize("x > ");
+		*y = InputSize("y > ");
+		cout << endl;
 	}
+	void Print() {
+		printf("x > %d\ny > %d\nsum > %d", *x, *y, *sum);
+	}
+	void CountS() {
+		*sum = *x + *y;
+		cout << sum << endl;
+	}
+	Sum() {
+		*x = 0;
+		*y = 0;
+		*sum = 0;
+	}
+	Sum(int x, int y) {
+		*this->x = x;
+		*this->y = y;
+		*sum = x + y;
+	}
+	/*Sum(int* x, int* y, int* sum = nullptr) {
+		this->x = x;
+		this->y = y;
+		*sum = *x + *y;
+		this->sum = sum;
+	}*/
 };
 
 int main()
 {
 	Standart();
-	Sum A;
-	A.Init();
+	int a = 5, b = 6;
+	Sum A(a, b);
+
+	//A.Init();
+	A.CountS();
 	A.Print();
-	return 0;
+
+	r0;
 }
