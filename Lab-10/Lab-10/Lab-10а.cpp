64e24c866f44713241f2e1117d9709f52776d031
Feest <ffgfvfgrgf@gmail.com>
2021-03-13 18:14:40 +0200

    reorganized

---
+++ Lab-10/Lab-10/Lab-10а.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+class Sq {
+public:
+	template <typename T>
+	T Square(T a, T b = '\0') {
+		if (b == '\0')
+			return a * a;
+		else
+			return a * b;
+	}
+};
+
+int main()
+{
+	Sq A;
+	cout << A.Square(6);
+	return 0;
+}
---
+++ Lab-10/Lab-10г/Lab-10г.cpp
@@ -0,0 +1,34 @@
+﻿#include "..\..\Header\Header.h"
+
+class Timer {
+	int sec;
+public:
+
+	Timer(int sec = 0) {
+		this->sec = sec;
+	};
+
+	Timer(int min, int sec) {
+		this->sec = sec + min * 60;
+	};
+
+	void Print() {
+		cout << sec << endl;
+	}
+	void SetS(int value = 0) {
+		sec = value;
+	}
+
+	Timer(const Timer& old) {
+		this->sec = old.sec;
+	};
+};
+
+int main()
+{
+	Timer A(6), B;
+	//A.SetS(5);
+	B = A;
+	B.Print();
+	r0;
+}
