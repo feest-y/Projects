6a8e4d9fd81903fb4de745f996ff6be3371e24b1
Feest <ffgfvfgrgf@gmail.com>
2021-03-13 19:35:31 +0200

    done?

--- Lаb-11/Lаb-11б/Lаb-11б.cpp
+++ Lаb-11/Lаb-11б/Lаb-11б.cpp
@@ -1,6 +1,42 @@
-﻿#include <iostream>
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+
+class Figure
+{
+	unsigned int A, B, C, R;
+public:
+	Figure(unsigned int R) {
+		this->R = R;
+		A = B = C = 0;
+	};
+
+	Figure() {
+		A = B = C = R = 0;
+	};
+
+	Figure(unsigned int A, unsigned int B, unsigned int C, unsigned int R = 0) {
+		this->A = A;
+		this->B = B;
+		this->C = C;
+		this->R = R;
+	};
+
+	double SquareOfTriangle() {
+		float p = (A + B + C) / 2;
+		return sqrt((p * (p - A) * (p - B) * (p - C)));
+	}
+
+	double SquareOfCircle() {
+		return M_PI * R * R;
+	}
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Figure A(3,2,3,4);
+	std::cout <<"SquareOfTriangle > "<< A.SquareOfTriangle()<<std::endl;
+	std::cout << "SquareOfCircle > " << A.SquareOfCircle();
+	return 0;
 }
