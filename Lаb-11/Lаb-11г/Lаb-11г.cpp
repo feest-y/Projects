19bb08518e36cdad46b2a4bbe937c3f9f1cd5ab6
Feest <ffgfvfgrgf@gmail.com>
2021-03-14 16:04:18 +0200

    init

--- Lаb-11/Lаb-11г/Lаb-11г.cpp
+++ Lаb-11/Lаb-11г/Lаb-11г.cpp
@@ -2,10 +2,27 @@
 using namespace std;
 
 
-class Master{
+class Master {
+
+	class Dog {
+		string name;
+		int weight, age;
 
 
+	public:
+		Dog() {
+			name = "Noname";
+			weight = age = 0;
+		}
+		Dog(string name,int weight,int age) {
+			this->name = name;
+			this->weight = weight;
+			this->age = age;
+		}
 
+
+	};
+public:
 
 };
 
